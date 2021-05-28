b988364236bcbde87c6a76377226cd475b26f2e6
Shiming Zhang <wzshiming@foxmail.com>
2021-05-28 10:36:23 +0800

    Add label format

--- bar.go
+++ bar.go
@@ -339,7 +339,9 @@ func (c *BarChart) addLabel(bar *Barinfo, y float64) {
 	}
 
 	var sval string
-	if math.Abs(y) >= 100 {
+	if c.YRange.TicSetting.Format != nil {
+		sval = c.YRange.TicSetting.Format(y)
+	}else if math.Abs(y) >= 100 {
 		sval = fmt.Sprintf("%d", int(y+0.5))
 	} else if math.Abs(y) >= 10 {
 		sval = fmt.Sprintf("%.1f", y)
